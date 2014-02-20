54ee8aad7fae12a09a8132ca2d11f2550dc98f68
Subhendu <subhendu@buffalo.edu>
2014-02-19 23:42:07 -0500

    Subhendu

--- HelloWorld/src/edu/buffalo/cse562/model/data/ResultSet.java
+++ HelloWorld/src/edu/buffalo/cse562/model/data/ResultSet.java
@@ -11,8 +11,22 @@ public class ResultSet {
 	public ArrayList<String> schema;
 	public ArrayList<Tuple> tuples;
 	
+	Iterator it;
+	
 	public ResultSet(ArrayList<String> a,ArrayList<Tuple> b){
 		schema = a;
 		tuples = b;
+		it = tuples.iterator();
+	}
+	
+	public boolean hasNext(){
+		if(it.hasNext())
+			return true;
+		else
+			return false;
+	}
+	
+	public Tuple next(){
+		return (Tuple)it.next();
 	}
 }
