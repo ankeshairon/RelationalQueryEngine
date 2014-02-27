79e707816f5a905cf92b1bad7f916e5127a75535
cse562 <dbharadw@buffalo.edu>
2014-02-26 23:55:37 -0500

    Push

--- HelloWorld/src/edu/buffalo/cse562/Main.java
+++ HelloWorld/src/edu/buffalo/cse562/Main.java
@@ -15,8 +15,9 @@ public class Main {
 
     /**
      * @param args
+     * @throws IOException 
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         int i;
         File dataDir = null;
@@ -24,7 +25,8 @@ public class Main {
         for (i = 0; i < args.length; i++) {
             if (args[i].equals("--data")) {
             	System.out.println(System.getProperty("user.dir") + "/" + args[i+1]);
-                dataDir = new File(System.getProperty("user.dir") + "/" + args[i + 1]);
+            	String current = new java.io.File( "." ).getCanonicalPath();
+                dataDir = new File(current + "/" + args[i + 1]);
                 i++;
             } else {
                 sqlFiles.add(new File(args[i]));
