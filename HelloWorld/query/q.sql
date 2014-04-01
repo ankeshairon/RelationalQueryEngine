1f3e288fa2b738a3b1f35964672db10b8c8f5ef8
cse562 <dbharadw@buffalo.edu>
2014-04-01 18:25:04 -0400

    Added tpch queries and tpch data

---
+++ HelloWorld/query/q.sql
@@ -0,0 +1,16 @@
+CREATE TABLE table1(
+sl1 int,
+name1 string,
+avg1 float
+)
+CREATE TABLE table2(
+sl2 int,
+name2 string,
+avg2 float
+)
+CREATE TABLE table3(
+sl3 int,
+name3 string,
+avg3 float
+)
+SELECT * FROM table1,table2,table3 WHERE avg2-avg1>1 and name1='Ramesh';
