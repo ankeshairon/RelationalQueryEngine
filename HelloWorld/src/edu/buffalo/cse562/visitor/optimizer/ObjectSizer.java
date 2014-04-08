5024fd37e377b62dbfd57b04697080236bb851ad
ankesh@mindreader <ankeshai@buffalo.edu>
2014-04-08 13:44:29 -0400

    Ankesh - adding object sizer

---
+++ HelloWorld/src/edu/buffalo/cse562/visitor/optimizer/ObjectSizer.java
@@ -0,0 +1,16 @@
+package edu.buffalo.cse562.visitor.optimizer;
+
+import java.lang.instrument.Instrumentation;
+
+public class ObjectSizer {
+
+    private static Instrumentation instrumentation;
+
+    public static void premain(String args, Instrumentation inst) {
+        instrumentation = inst;
+    }
+
+    public static long getObjectSize(Object o) {
+        return instrumentation.getObjectSize(o);
+    }
+}
