5eacf2375fba972fc1d8ec496be556e6a687a391
ankesh@mindreader <ankeshai@buffalo.edu>
2014-05-01 16:02:38 -0400

    Ankesh - Filescanner placeholder

---
+++ HelloWorld/src/edu/buffalo/cse562/operator/FileScanner.java
@@ -0,0 +1,14 @@
+package edu.buffalo.cse562.operator;
+
+import java.io.File;
+
+public class FileScanner {
+
+    public FileScanner(File dataDir, String tableName) {
+
+    }
+
+    public String readOneLine(){
+        return null;
+    }
+}
