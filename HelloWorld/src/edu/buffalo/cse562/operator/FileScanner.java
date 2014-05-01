82a2624fd27aa2e3f9a4352728d529d82eaf161f
ankesh@mindreader <ankeshai@buffalo.edu>
2014-05-01 16:18:10 -0400

    Ankesh - Filescanner placeholder

--- HelloWorld/src/edu/buffalo/cse562/operator/FileScanner.java
+++ HelloWorld/src/edu/buffalo/cse562/operator/FileScanner.java
@@ -1,14 +1,21 @@
 package edu.buffalo.cse562.operator;
 
+import edu.buffalo.cse562.indexer.model.TableIndexingInfo;
+import edu.buffalo.cse562.schema.ColumnSchema;
+
 import java.io.File;
 
 public class FileScanner {
 
-    public FileScanner(File dataDir, String tableName) {
+    public FileScanner(File dataDir, TableIndexingInfo tableIndexingInfo) {
 
     }
 
-    public String readOneLine(){
+    public String readOneLine() {
+        return null;
+    }
+
+    public ColumnSchema[] getSchema() {
         return null;
     }
 }
